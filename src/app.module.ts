35339be6947c5013a3c0add75e1b2ef792061eb5
daniyalkarim0340 <daniyalkarim70@gmail.com>
2026-08-17 12:30:27 +0500

    connect adat base

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,13 @@ import { CategeoryModule } from './categeory/categeory.module';
 import { StudentsModule } from './students/students.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [EmployeeModule, CategeoryModule, StudentsModule, AuthModule, UsersModule],
+  imports: [EmployeeModule, CategeoryModule, StudentsModule, AuthModule, UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string)
+  ],
   controllers: [AppController, UserController, ProductController],
   providers: [AppService, ProductService],
 })
